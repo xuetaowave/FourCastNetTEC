9ba06819780f97cf8fa6f3d6149e6c75cb4abbaa
xuetaowave <cxt@cxt-win>
2024-02-18 20:21:15 +0800

    update

--- train.py
+++ train.py
@@ -559,7 +559,7 @@ if __name__ == '__main__':
   local_rank = 0
   if params['world_size'] > 1:
     dist.init_process_group(backend='nccl',
-                            init_method='env://', timeout=timedelta(seconds=10))
+                            init_method='env://', timeout=timedelta(seconds=120))
     local_rank = int(os.environ["LOCAL_RANK"])
     args.gpu = local_rank
     world_rank = dist.get_rank()
