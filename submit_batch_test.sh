107f969c1b0acdfe0eae5c3b7871f3ce313cb988
xuetaowave <cxt@cxt-win>
2024-02-19 12:59:35 +0800

    update

---
+++ submit_batch_test.sh
@@ -0,0 +1,24 @@
+-#!/bin/bash -l
+#SBATCH --time=01:00:00
+#SBATCH -J afno
+#SBATCH -o afno_backbone_finetune.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:a100:2 -p GPU-8A100 --qos=gpu_8a100
+
+config_file=./config/AFNO.yaml
+config='afno_backbone_tec_ustc'
+run_num='1'
+
+export HDF5_USE_FILE_LOCKING=FALSE
+export NCCL_NET_GDR_LEVEL=PHB
+
+export MASTER_ADDR=$(hostname)
+
+set -x
+srun -u --mpi=pmi2 \
+    bash -c "
+    source export_DDP_vars.sh
+    python train.py --enable_amp --yaml_config=$config_file --config=$config --run_num=$run_num
+    "
