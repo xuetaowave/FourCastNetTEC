b8793df36af42f23f261df60fa9eb282776115fb
xuetaowave <cxt@cxt-win>
2024-02-19 16:53:13 +0800

    update

---
+++ data_ljkj/collect_data.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+from datetime import datetime, timedelta
+from time import time
+import os 
+os.getcwd()
+
+
+# In[2]:
+
+
+start_date = datetime(2010, 1, 11)
+end_date = datetime(2010, 12, 31)
+
+
+# In[3]:
+
+
+kind = 'Density'
+value = []
+date_list = []
+for i in range((end_date-start_date).days):
+    t0 = time()
+    c_date = start_date+timedelta(i)
+    filename = '/home/cxt/data/work/Data/WACCM/{}/{}/FWSD_2010.cam.h1.{}-00000.{}.2-5km.npz'.format(c_date.year, kind, c_date.strftime('%Y-%m-%d'), kind)
+
+    seconds = np.arange(0, 24*60*60, 60*30)
+    date_i = np.array([c_date+timedelta(0, int(seconds[i])) for i in range(seconds.size)])
+
+    if os.path.exists(filename) is True:
+        with np.load(filename) as data_load:
+            value.append(np.array(data_load['value'][:, -1].astype('float32')))      
+        date_list.append(date_i)
+        t1 = time()
+        print('time usage:{}   finish:{}'.format(t1-t0, filename))
+    else:
+        print('not found: {}'.format(filename))
+value_all = np.concatenate(value, axis=0)
+np.savez(kind, fields=value_all, date=np.array(date_list))
+pass
+
+
+# In[5]:
+
+
+kind = 'T'
+value = []
+date_list = []
+for i in range((end_date-start_date).days):
+    t0 = time()
+    c_date = start_date+timedelta(i)
+    filename = '/media/cxt/Elements/WACCM/{}/{}_height/FWSD_2010.cam.h1.{}-00000.{}.2-5km.npz'.format(c_date.year, kind, c_date.strftime('%Y-%m-%d'), kind)
+
+    seconds = np.arange(0, 24*60*60, 60*30)
+    date_i = np.array([c_date+timedelta(0, int(seconds[i])) for i in range(seconds.size)])
+
+    if os.path.exists(filename) is True:
+        with np.load(filename) as data_load:
+            value.append(np.array(data_load['value'][:, -1].astype('float32')))      
+        date_list.append(date_i)
+        t1 = time()
+        print('time usage:{}   finish:{}'.format(t1-t0, filename))
+    else:
+        print('not found: {}'.format(filename))
+value_all = np.concatenate(value, axis=0)
+np.savez(kind, fields=value_all, date=np.array(date_list))
+
+
+# In[7]:
+
+
+kind = 'U'
+value = []
+date_list = []
+for i in range((end_date-start_date).days):
+    t0 = time()
+    c_date = start_date+timedelta(i)
+    filename = '/media/cxt/Elements/WACCM/{}/{}_height/FWSD_2010.cam.h1.{}-00000.{}.2-5km.npz'.format(c_date.year, kind, c_date.strftime('%Y-%m-%d'), kind)
+
+    seconds = np.arange(0, 24*60*60, 60*30)
+    date_i = np.array([c_date+timedelta(0, int(seconds[i])) for i in range(seconds.size)])
+
+    if os.path.exists(filename) is True:
+        with np.load(filename) as data_load:
+            value.append(np.array(data_load['value'][:, -1].astype('float32')))      
+        date_list.append(date_i)
+        t1 = time()
+        print('time usage:{}   finish:{}'.format(t1-t0, filename))
+    else:
+        print('not found: {}'.format(filename))
+value_all = np.concatenate(value, axis=0)
+np.savez(kind, fields=value_all, date=np.array(date_list))
+
+
+# In[9]:
+
+
+kind = 'V'
+value = []
+date_list = []
+for i in range((end_date-start_date).days):
+    t0 = time()
+    c_date = start_date+timedelta(i)
+    filename = '/media/cxt/Elements/WACCM/{}/{}_height/FWSD_2010.cam.h1.{}-00000.{}.2-5km.npz'.format(c_date.year, kind, c_date.strftime('%Y-%m-%d'), kind)
+
+    seconds = np.arange(0, 24*60*60, 60*30)
+    date_i = np.array([c_date+timedelta(0, int(seconds[i])) for i in range(seconds.size)])
+
+    if os.path.exists(filename) is True:
+        with np.load(filename) as data_load:
+            value.append(np.array(data_load['value'][:, -1].astype('float32')))      
+        date_list.append(date_i)
+        t1 = time()
+        print('time usage:{}   finish:{}'.format(t1-t0, filename))
+    else:
+        print('not found: {}'.format(filename))
+value_all = np.concatenate(value, axis=0)
+np.savez(kind, fields=value_all, date=np.array(date_list))
+
+
+# In[10]:
+
+
+kind = 'P'
+value = []
+date_list = []
+for i in range((end_date-start_date).days):
+    t0 = time()
+    c_date = start_date+timedelta(i)
+    filename = '/media/cxt/Elements/WACCM/{}/{}_height/FWSD_2010.cam.h1.{}-00000.lev.2-5km.npz'.format(c_date.year, kind, c_date.strftime('%Y-%m-%d'))
+
+    seconds = np.arange(0, 24*60*60, 60*30)
+    date_i = np.array([c_date+timedelta(0, int(seconds[i])) for i in range(seconds.size)])
+
+    if os.path.exists(filename) is True:
+        with np.load(filename) as data_load:
+            value.append(np.array(data_load['value'][:, -1].astype('float32')))      
+        date_list.append(date_i)
+        t1 = time()
+        print('time usage:{}   finish:{}'.format(t1-t0, filename))
+    else:
+        print('not found: {}'.format(filename))
+value_all = np.concatenate(value, axis=0)
+np.savez(kind, fields=value_all, date=np.array(date_list))
+
---
+++ data_ljkj/create_dataset.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import h5py 
+
+
+# In[6]:
+
+
+data_load_D = np.load('Density.npz')
+D = data_load_D['fields']
+
+data_load_P = np.load('P.npz')
+P = data_load_P['fields']
+
+data_load_T = np.load('T.npz')
+T = data_load_T['fields']
+
+data_load_U = np.load('U.npz')
+U = data_load_U['fields']
+
+data_load_V = np.load('V.npz')
+V = data_load_V['fields']
+
+list(data_load_D.keys()), D.shape, P.shape, T.shape, U.shape, V.shape
+
+# In[18]:
+
+
+data = np.hstack((D.reshape((-1, 6)+D.shape[1:]), P.reshape((-1, 6)+P.shape[1:]), T.reshape((-1, 6)+T.shape[1:]), U.reshape((-1, 6)+U.shape[1:]), V.reshape((-1, 6)+V.shape[1:])))
+train_mask = data.shape[0]//6*5
+
+
+# In[19]:
+
+
+import os
+print(os.getcwd())
+os.makedirs('train', exist_ok=True)
+os.makedirs('test', exist_ok=True)
+os.makedirs('out_of_sample', exist_ok=True)
+
+
+# In[20]:
+
+
+import h5py
+with h5py.File('train/2010.h5', 'w') as f:
+    f.create_dataset("fields", data=data[:train_mask])
+    
+with h5py.File('test/2010.h5', 'w') as f:
+    f.create_dataset("fields", data=data[train_mask:])
+    
+with h5py.File('out_of_sample/2010.h5', 'w') as f:
+    f.create_dataset("fields", data=data[train_mask:])
+
+
+# In[ ]:
+
+
+
+
